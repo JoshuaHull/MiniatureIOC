41d25fff5fb032afa2ef5db919d6f01b954585d0
JoshuaHull <36806782+JoshuaHull@users.noreply.github.com>
2020-04-10 13:06:02 +1000

    Initial Commit

---
+++ MiniatureIOC/Helpers/RegexHelp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MiniatureIOC.Helpers
+{
+    public class RegexHelp
+    {
+        public static bool IsMatch(string regexStr, string inputStr)
+        {
+            var regex = new Regex(regexStr);
+            Match match = regex.Match(inputStr);
+
+            return match.Success;
+        }
+    }
+}
